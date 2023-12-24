586a2be481984c83b73d59c85c092b5fa87682df
IvoGeorgievx <102153817+IvoGeorgievx@users.noreply.github.com>
2023-12-24 12:40:46 +0200

    playground

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService) {}
-    register(dto: AuthDto) {
-        return 'register user';
+    async register(dto: AuthDto) {
+        const hash = await argon.hash(dto.password);
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                },
+            });
+            delete user.hash;
+            return user;
+        } catch (error) {
+            if (error.code === 'P2002') {
+                // P2002 = unique constraint failed
+                throw new ForbiddenException('Email already exists');
+            }
+        }
     }
 
     login() {
