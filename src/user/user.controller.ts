3222136e64fe80a242b61a595f7cec72ff026525
IvoGeorgievx <102153817+IvoGeorgievx@users.noreply.github.com>
2023-12-27 19:27:32 +0200

    playground

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    HttpCode,
+    HttpStatus,
+    Req,
+    UseGuards,
+} from '@nestjs/common';
 import { Request } from 'express';
 import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UserController {
+    @HttpCode(HttpStatus.OK)
     @UseGuards(JwtGuard)
     @Get('me')
     getMe(@Req() req: Request) {
