429fc8ba37b139fd7a868f9f27f0294b063bd1f2
IvoGeorgievx <102153817+IvoGeorgievx@users.noreply.github.com>
2024-01-01 16:38:01 +0200

    playground

--- middlewares/logger.middleware.ts
+++ middlewares/logger.middleware.ts
@@ -4,7 +4,7 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
     use(req: Request, res: Response, next: NextFunction) {
-        console.log('Request...', req.method, req.originalUrl);
+        console.log('Incoming request: ', req.method, req.originalUrl);
         next();
     }
 }
