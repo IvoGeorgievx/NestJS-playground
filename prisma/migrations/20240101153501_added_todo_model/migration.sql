476380767800ebc54d5d6795bfed530581e9e831
IvoGeorgievx <102153817+IvoGeorgievx@users.noreply.github.com>
2024-01-01 17:36:10 +0200

    playground

---
+++ prisma/migrations/20240101153501_added_todo_model/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "todos" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "todos_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "todos" ADD CONSTRAINT "todos_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
